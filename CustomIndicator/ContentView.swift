44b31159ff4ef2b288dc1fa2e4ecc96b6a010248
Dhanush <dhanush@dhanush.corp.capgemini.com>
2020-01-28 12:01:13 +0530

    Local Changes

--- CustomIndicator/ContentView.swift
+++ CustomIndicator/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        Loader()
     }
 }
 
@@ -19,3 +19,22 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Loader : View {
+    @State var animate = false
+    var body : some View {
+        VStack{
+        
+        Circle().trim(from: 0, to: 0.8)
+            .stroke(AngularGradient(gradient : .init(colors :[.red,.orange]) , center : .center), style : StrokeStyle(lineWidth : 8, lineCap : .round))
+        .frame(width: 45, height: 45)
+            .rotationEffect(.init(degrees: self.animate ? 360 : 0))
+            .animation(Animation.linear(duration: 0.7))
+           .repeatForever(autoreverses: false))
+        }.onAppear{
+            self.animate.toggle()
+        }
+    }
+}
+ 
+
